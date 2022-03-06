19f095a666bc45843b394d4a4f0addcff1f3ae0b
Firebreak-2 <69664260+Firebreak-2@users.noreply.github.com>
2022-03-06 14:54:44 +0400

    Fixed some issues. Added the per-server databases instead of a shared database.

--- Program.cs
+++ Program.cs
@@ -7,31 +7,18 @@ public static class Program
 {
     public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
     public static DiscordSocketClient Client = null!;
-    public static Dictionary<string, string> TextCommands = new();
-    private const string CommandsTextPath = "commands.txt";
     public static async Task MainAsync()
     {
         Client = new DiscordSocketClient(new DiscordSocketConfig());
         Client.Log += Log;
         Client.Ready += Initialize;
         Client.MessageReceived += OnMessageRecieved;
+        Client.JoinedGuild += OnGuildJoined;
 
-        /* Initialize */
+        foreach (var guild in Client.Guilds)
         {
-            if (!File.Exists(CommandsTextPath))
-            {
-                File.Create(CommandsTextPath);
-                Thread.Sleep(1000);
-            }
-            foreach (var item in await File.ReadAllLinesAsync(CommandsTextPath))
-            {
-                if (item == "") continue;
-                
-                var split = item.Split(':');
-                TextCommands.Add(split[0], split[1].Replace("\\n", "\n"));
-            }
+            CreateGuildCommandsFile(guild);
         }
-
 
         const string path = "token.txt";
         string token;
@@ -55,6 +42,17 @@ public static class Program
         return Task.CompletedTask;
     }
 
+    private static Task OnGuildJoined(SocketGuild guild)
+    {
+        CreateGuildCommandsFile(guild);
+        return Task.CompletedTask;
+    }
+
+    private static void CreateGuildCommandsFile(SocketGuild guild)
+    {
+        if (!File.Exists($"{guild.Id}"))
+            File.WriteAllText($"{guild.Id}", "");
+    }
     private static Task OnMessageRecieved(SocketMessage message)
     {
         const string prefix = "!";
@@ -62,24 +60,39 @@ public static class Program
 
         if (message.Content.StartsWith($"{prefix}addcmd"))
         {
+            
             var split = message.Content.Split(' ').Where((_, i) => i != 0).ToArray();
 
             string input = split[0];
             string output = string.Join(" ", split.Where((_, i) => i != 0));
-            
-            AddCommandToFile(input, output);
-            message.Channel.SendMessageAsync($"Added command `{input}` with output: `{output.Replace("\\n", "\n")}`");
+
+            message.Channel.SendMessageAsync(
+                AddCommandToFile(message.Channel.GetGuild(), input, output)
+                    ? $"Added command `{input}` with output: `{output.Replace("\\n", "\n")}`"
+                    : $"Command `{input}` is already present.");
+        }
+        else if (message.Content.StartsWith($"{prefix}rmvcmd"))
+        {
+            var input = message.Content.Split(' ').Where((_, i) => i != 0).ToArray()[0];
+            var guild = message.Channel.GetGuild();
+
+            if (input is "addcmd" or "clrcmds" or "rmvcmd")
+                message.Channel.SendMessageAsync("Cannot remove this command.");
+            else
+                message.Channel.SendMessageAsync(RemoveCommandToFile(guild, input)
+                    ? $"Removed command `{input}`."
+                    : $"No command named `{input}` found.");
         }
         else switch (message.Content)
         {
             case $"{prefix}clrcmds":
-                ClearCommands();
+                ClearCommands(message.Channel.GetGuild());
                 message.Channel.SendMessageAsync("Cleared all commands.");
                 break;
             case $"{prefix}help":
                 message.Channel.SendMessageAsync(null, false, new EmbedBuilder()
                     .WithTitle("All commands")
-                    .WithDescription(string.Join("\n", TextCommands.Select(x => $"{x.Key}:{x.Value.Replace("\n", "\\n")}")) + "\n" + string.Join("\n", "clrcmds:Clears all user-set commands.", "addcmd:Adds a new command. (`!addcmd example-cmd this is an example`)"))
+                    .WithDescription(string.Join("\n", File.ReadAllLines($"{message.Channel.GetGuild().Id}")) + "\n" + string.Join("\n", "clrcmds:Clears all user-set commands.", "addcmd:Adds a new command. (`!addcmd example-cmd this is an example`)", "rmvcmd:Removes a specified command. (`!rmvcmd example-cmd`)"))
                     .WithColor(Color.Blue)
                     .Build());
                 break;
@@ -88,7 +101,7 @@ public static class Program
                 if (message.Content.StartsWith(prefix) && message.Content.Length > 1)
                 {
                     var cmdName = message.Content.Remove(0, 1);
-                    message.Channel.SendMessageAsync(TextCommands.TryGetValue(cmdName, out var output)
+                    message.Channel.SendMessageAsync(GetCommandFromFile(message.Channel.GetGuild(), cmdName, out var output)
                         ? output
                         : $"No command named `{cmdName}` found.");
                 }
@@ -99,17 +112,49 @@ public static class Program
         return Task.CompletedTask;
     }
 
-    private static void AddCommandToFile(string command, string output)
+    private static bool AddCommandToFile(SocketGuild guild, string command, string output)
     {
-        TextCommands.Add(command, output.Replace("\\n", "\n"));
-        var lines = File.ReadAllText(CommandsTextPath);
-        File.WriteAllText(CommandsTextPath, lines + $"\n{command}:{output.Replace("\n", "\\n")}");
+        CreateGuildCommandsFile(guild);
+        
+        var lines = File.ReadAllText($"{guild.Id}");
+        var lines2 = lines.Split('\n');
+        
+        if (lines2.Any(x => x.Split(':')[0] is "addcmd" or "clrcmds" or "rmvcmd" || x.Split(':')[0] == command)) return false;
+        
+        File.WriteAllText($"{guild.Id}", lines + $"\n{command}:{output.Replace("\n", "\\n")}");
+        return true;
+    }
+    private static bool RemoveCommandToFile(SocketGuild guild, string command)
+    {
+        CreateGuildCommandsFile(guild);
+        
+        var lines = File.ReadAllLines($"{guild.Id}");
+        
+        var condition = new Func<string, bool>(x => x.Split(':')[0] != command);
+        
+        if (lines.All(condition)) return false;
+        
+        File.WriteAllLines($"{guild.Id}", lines.Where(condition));
+        return true;
     }
 
-    private static void ClearCommands()
+    private static bool GetCommandFromFile(SocketGuild guild, string command, out string response)
     {
-        TextCommands.Clear();
-        File.WriteAllText(CommandsTextPath, "");
+        CreateGuildCommandsFile(guild);
+        
+        var lines = File.ReadAllLines(guild.Id.ToString());
+        var condition = new Func<string, bool>(x => x.Split(':')[0] == command);
+        if (lines.Any(condition))
+        {
+            response = lines.Single(condition).Split(':')[1];
+            return true;
+        }
+        response = "null";
+        return false;
+    }
+    private static void ClearCommands(SocketGuild guild)
+    {
+        File.WriteAllText(guild.Id.ToString(), "");
     }
     private static Task Initialize()
     {
